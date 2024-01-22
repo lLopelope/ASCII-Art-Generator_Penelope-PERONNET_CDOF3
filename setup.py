80aa426a1975aac81a1c4619b0d2a045a2087b12
unknown <penelope.peronnet@edu.devinci.fr>
2024-01-22 18:04:16 +0100

    ajout setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{ASCII-Art-Generator_Penelope-PERONNET_CDOF3}',
+ version='1.0',
+ author='{lLopelope}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
