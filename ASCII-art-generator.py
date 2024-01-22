bd1d5899b84e74503803cce401cb1ebe41f7da8f
unknown <penelope.peronnet@edu.devinci.fr>
2024-01-22 19:10:27 +0100

    modification app

--- ASCII-art-generator.py
+++ ASCII-art-generator.py
@@ -1,9 +1,9 @@
 import pyfiglet
 
-def generate_ascii_art(text):
+def ascii_converter(text):
     ascii_art = pyfiglet.figlet_format(text)
     print(ascii_art)
 
-if __name__ == "__main__":
-    user_input = input("Enter text for ASCII art: ")
-    generate_ascii_art(user_input)
+text =input("saisir le texte à convertir :")
+ascii_converter(text)
+    
